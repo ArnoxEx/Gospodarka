2847fd4a333eee9417be0b3b14d50b8ff000ff43
solanik <solanik@wp.pl>
2016-01-09 18:06:10 +0100

    add ignored files

--- Gospodarka/settings.py
+++ Gospodarka/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Gospodarka.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'OracleDatabase',
+        'NAME': 'STUDIA',
         'USER': 'gospodarrz',
         'PASSWORD': 'kochamJule1',
     }
