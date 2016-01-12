0cad92d1be5ab1e8721becc124c36534b3bf2395
ArnoxEx <kamil5108@gmail.com>
2016-01-12 22:45:14 +0100

    Addition of jQuery to project

--- Gospodarka/settings.py
+++ Gospodarka/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 	'Gospodarka.gospodarkaApp',
     'jquery',
+    'django_static_jquery',
 ]
 
 MIDDLEWARE_CLASSES = [
--- Gospodarka/static/static_dirs/js/Hello.js
+++ Gospodarka/static/static_dirs/js/Hello.js
@@ -1,1 +1,7 @@
 alert('Hello World');
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
