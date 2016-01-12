2a6b2775b8527e6f5dd8496d497df8a5189ab23a
ArnoxEx <kamil5108@gmail.com>
2016-01-12 15:42:28 +0100

    Creation of static files directory hierarchy

--- Gospodarka/gospodarkaApp/urls.py
+++ Gospodarka/gospodarkaApp/urls.py
@@ -3,11 +3,11 @@ from Gospodarka.gospodarkaApp.views import *
 
 urlpatterns = patterns('',
 #url(r'^(?P<emp_id>\d+)/(?P<dept_name>\w+)/(?P<dept_id>\d+)/$' , newdept),
-url(r'^$',          index),
+url(r'^$',          index,          name='index'),
 url(r'^register',   register,       name='register'),
 url(r'^login',      user_login,     name='login'),
 url(r'^logout',     user_logout,    name='logout'),
 url(r'^objects',    objects,        name='objects'),
 url(r'^add_object', add_object,     name='add_object'),
-url(r'^events', eventsTable,     name='add_object'),
+url(r'^events', eventsTable,        name='add_object'),
 )
