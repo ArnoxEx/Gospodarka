bff23daec61ab139523cd0182821336e7c2b49e0
ArnoxEx <kamil5108@gmail.com>
2016-01-13 23:06:07 +0100

    addition of event table sort, filter

--- Gospodarka/static/static_root/js/Hello.js
+++ Gospodarka/static/static_root/js/Hello.js
@@ -1,7 +1,35 @@
-alert('Hello World');
+    var table = $('table');
+    alert("Dupa1");
+    $("#dupa").css( "border", "3px solid red" );
+    $( ".sortable" ).css( "border", "3px solid red" );
+    $(".sortable")
+        .wrapInner('<span title="sort this column"/>')
+        .each(function(){
+            var th = $(this),
+                thIndex = th.index(),
+                inverse = false;
 
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
-});
+            th.click(function(){
+                alert("Dupa");
+                table.find('td').filter(function(){
+
+                    return $(this).index() === thIndex;
+
+                }).sortElements(function(a, b){
+
+                    return $.text([a]) > $.text([b]) ?
+                        inverse ? -1 : 1
+                        : inverse ? 1 : -1;
+
+                }, function(){
+
+                    // parentNode is the element we want to move
+                    return this.parentNode;
+
+                });
+
+                inverse = !inverse;
+
+            });
+
+        });
